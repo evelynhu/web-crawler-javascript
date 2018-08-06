65da6de0410e83f2371a372cb8841d2f41c7a156
Evelyn Hu <befuner@gmail.com>
2018-08-06 22:18:45 +0800

    sort string

--- crawler.js
+++ crawler.js
@@ -7,6 +7,14 @@ const BASE_URL = 'https://maicoin2.freshdesk.com/';
 const SUB_URL = '/support/solutions/articles/';
 const ARTICLE_URL_PAGE_1 = 'https://maicoin2.freshdesk.com/support/solutions/folders/32000034455';
 const ARTICLE_URL_PAGE_2 = 'https://maicoin2.freshdesk.com/support/solutions/folders/32000034455/page/2';
+const coinpedia = {
+  en: {},
+  'zh-TW': {},
+};
+const sortedCoinpedia = {
+  en: {},
+  'zh-TW': {},
+};
 
 const fetch = (url) => {
     console.log('Processing', url);
@@ -32,37 +40,62 @@ const parseURL = ($, articles) => {
 };
 
 const crawl = (url, locale) => {
-  request(url, function(error, response, body) {
-    if(error) {
-      console.log("Error: " + error, '@', url);
-    }
-    console.log("Status code: " + response.statusCode, '@', url);
+  return new Promise(function (resolve, reject) {
+    request(url, function(error, response, body) {
+      if(error) {
+        console.log("Error: " + error, '@', url);
+        reject(error);
+      }
+      console.log("Status code: " + response.statusCode, '@', url);
 
-    var $ = cheerio.load(body);
-    const title = $('header.article-header').find('h1').text().trim();
-    const symbol = title.match(/\(([^)]+)\)/);
-    let currency = title.toLowerCase();
-    if (symbol) {
-      currency = symbol[1].trim().toLowerCase();
-    }
-    let introduction = '';
-    $('div.article__body').find('p').each(function(index) {
-       introduction += $(this).text().replace('\'', '\\\'') + '\\n\\n';
-   });
+      var $ = cheerio.load(body);
+      const title = $('header.article-header').find('h1').text().trim();
+      const symbol = title.match(/\(([^)]+)\)/);
+      let currency = title.toLowerCase();
+      if (symbol) {
+        currency = symbol[1].trim().toLowerCase();
+      }
+      let introduction = '';
+      $('div.article__body').find('p').each(function(index) {
+         introduction += $(this).text().replace('\'', '\\\'') + '\\n\\n';
+     });
 
-    if (currency && introduction) {
-      fs.appendFileSync(`coinpedia_${locale}.txt`, `${currency}: '${introduction}',\n`);
-    }
+      if (currency && introduction) {
+        coinpedia[locale][currency] = introduction;
+        // fs.appendFileSync(`coinpedia_${locale}.json`, `${currency}: '${introduction}',\n`);
+      }
+      resolve(body);
+    });
   });
 };
 
 const parseArticles = articles => {
-  articles.map(id => {
-    languages.map(locale => {
+  Promise.all(articles.map(id => {
+    return Promise.all(languages.map(locale => {
       const url = `${BASE_URL}${locale}${SUB_URL}${id}`;
       console.log('Visit ', url);
-      crawl(url, locale);
-    })
+      return crawl(url, locale);
+    }));
+  })).then(() => {
+    languages.map(locale => {
+      Object.keys(coinpedia[locale]).sort().forEach((key) => {
+        sortedCoinpedia[locale][key] = coinpedia[locale][key];
+      });
+      fs.writeFile(`coinpedia_${locale}.json`, JSON.stringify(sortedCoinpedia[locale], null, 2) + '\r\n',
+        (error) => {
+          if (error) {
+            return console.log(err);
+          } else {
+            console.log('success.!');
+          }
+        });
+    });
+  });
+};
+
+const printResult = () => {
+  languages.map(locale => {
+    console.log(JSON.parse(fs.readFileSync(`coinpedia_${locale}.json`).toString()));
   });
 };
 
@@ -83,6 +116,7 @@ const main = () => {
     .then(articles => {
       console.log(articles);
       parseArticles(articles);
+      printResult();
     });
   });
 }
